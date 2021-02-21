04439e3ee34247b63a6e1636eec0b8641920dbf9
sithankanna <sithankanna@gmail.com>
2021-02-21 15:08:34 +0000

    Add initial files for hierarchical models

---
+++ misc/hierarchical_bayesian/feature_factory.py
@@ -0,0 +1,86 @@
+import pandas as pd
+
+
+class FeatureFactory:
+    def __init__(self, date_col: str = None) -> None:
+        """ """
+
+        self.date_col = date_col
+        if date_col is None:
+            self.date_col = "date"
+        else:
+            self.date_col = date_col
+
+        self.features = []
+
+    def calendar_features(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Add calendar related features
+
+        Parameters:
+            df (pd.DataFrame): DataFrame with a date columns
+        Returns:
+            pd.DataFrame: Data .
+
+
+        """
+        date = df[self.date_col]
+
+        calendar_features = {
+            "day_of_week": self.day_of_week(date),
+            "week_of_month": self.week_of_month(date),
+            "week_of_year": self.week_of_year(date),
+            "month": self.month(date),
+        }
+
+        df = df.assign(**calendar_features)
+
+        self.features += list(calendar_features.keys())
+        return df
+
+    def week_of_month(self, date: pd.Series) -> pd.Series:
+        """Get week number in a month.
+
+        Parameters:
+            dates (pd.Series): Series of dates.
+
+        Returns:
+            pd.Series: Week number in a month.
+        """
+        firstday_in_month = date - pd.to_timedelta(date.dt.day - 1, unit="d")
+        return (date.dt.day - 1 + firstday_in_month.dt.weekday) // 7 + 1
+
+    def week_of_year(self, date: pd.Series) -> pd.Series:
+        """Get week number in a month.
+
+        Parameters:
+            date (pd.Series): Series of dates.
+
+        Returns:
+            pd.Series: Week number in a year.
+        """
+
+        return date.dt.week
+
+    def month(self, date: pd.Series) -> pd.Series:
+        """Get week number in a month.
+
+        Parameters:
+            date (pd.Series): Series of dates.
+
+        Returns:
+            pd.Series: Month number in a year.
+        """
+
+        return date.dt.month
+
+    def day_of_week(self, date: pd.Series) -> pd.Series:
+        """Get week number in a month.
+
+        Parameters:
+            date (pd.Series): Series of dates.
+
+        Returns:
+            pd.Series: Month number in a year.
+        """
+
+        return date.dt.dayofweek
